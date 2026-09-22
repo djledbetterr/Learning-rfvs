fb00a37f6065bcdd8544535c3db9f218be8998d8
Darius Ledbetter <dariusjbetter12king@gmail.com>
2026-09-22 16:58:40 -0400

    inital commit :)

---
+++ learnign rfvs.R
@@ -0,0 +1,6 @@
+# okay so this script is just me working through and learnign rfvs since there is so much information about this
+
+# lets start with installing this 
+
+sdir = "<FVS-interface>/rFVS/R/R-4.x.x/library/rFVS"
+for (rf in dir (sdir)) source (paste(sdir,rf,sep="/"))
